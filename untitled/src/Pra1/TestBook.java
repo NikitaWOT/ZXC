c6157ca94b879feb95bc06681db4ec8a844ecd0d
NikitaWOT <92212874+NikitaWOT@users.noreply.github.com>
2021-10-09 15:15:24 +0300

    Add files via upload

---
+++ untitled/src/Pra1/TestBook.java
@@ -0,0 +1,12 @@
+package Pra1;
+public class TestBook {
+    public static void main(String[] args)
+    {
+        Book b1 = new Book("Romance", 560, "Pushkin");
+        Book b2 = new Book("Fantasy", 800);
+        b2.setAuthor("Pushkin");
+        System.out.println(b1);
+        System.out.println(b1.output());
+        System.out.println(b2.output());
+    }
+}
